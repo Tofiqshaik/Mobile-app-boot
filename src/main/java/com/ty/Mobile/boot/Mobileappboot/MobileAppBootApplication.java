47869b463d8ed5b238656efd081b7e386e3bd3b1
Toufiq <tofiqshaik1.com>
2022-03-29 09:28:45 +0530

    code for mobile app

---
+++ src/main/java/com/ty/Mobile/boot/Mobileappboot/MobileAppBootApplication.java
@@ -0,0 +1,13 @@
+package com.ty.Mobile.boot.Mobileappboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/Mobile/boot/Mobileappboot/MobileAppBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.Mobile.boot.Mobileappboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
